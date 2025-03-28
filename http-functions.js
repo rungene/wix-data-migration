6177460d5de447461a68272641a67c5005d928a7
rungene <rungenesamuel@gmail.com>
2025-03-28 01:29:09 +0000

    Fixing data fetch to return all the data

--- http-functions.js
+++ http-functions.js
@@ -1,34 +1,37 @@
 // Expose your site's functionality externally
 // Follow instructions hear: https://dev.wix.com/docs/develop-websites/articles/coding-with-velo/integrations/exposing-services/write-an-http-function
 // to your Wix site Backend. Add the following in a file called 'http-functions.js'
-
 import { ok, notFound, serverError } from "wix-http-functions";
-import wixData  from "wix-data";
+import wixData from "wix-data";
 
-export function get_shoesListing(request) {
+export async function get_storeListing(request) {
     let options = {
         "headers": {
             "Content-Type": "application/json"
         }
     };
-    // Data you are interested with. All store products in my case
-    let productsQuery = wixData.query("Stores/Products");
 
-    return productsQuery
-        .find()
-        .then((results)=> {
-            if (results.items.length > 0) {
-                options.body = {
-                    "items": results.items
-                }
-                return ok(options);
-            }
-            return notFound(options)
-        })
-        .catch((error)=> {
-            options.body = {
-                "error": error.message
-            };
-            return serverError(options);
-        });
+    try {
+        let allItems = [];
+        let results = await wixData.query("Stores/Products").limit(100).find();
+
+        allItems = allItems.concat(results.items);
+
+        // Keep fetching next pages until all data is retrieved
+        while (results.hasNext()) {
+            results = await results.next();  // Await each next page
+            allItems = allItems.concat(results.items);
+        }
+
+        if (allItems.length > 0) {
+            options.body = { "items": allItems };
+            return ok(options);
+        }
+
+        return notFound(options);
+
+    } catch (error) {
+        options.body = { "error": error.message };
+        return serverError(options);
+    }
 }
